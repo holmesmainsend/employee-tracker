d11001a8cb375161f1cc1d4601581626c995319b
Mona Lisa <holmesmainsend@gmail.com>
2021-12-11 13:12:46 -0500

    added mysql2

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
-function generalPrompts() {
-    inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'home',
-            message: 'Employee Tracker Home Screen',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update Employee Role']
-        }
-    ])
-    .then((data) => {
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'employee_tracker'
+    },
+    console.log('Connected to the employee_tracker database.')
+  );
 
-    })
-}
+  db.query(
+      'source db/schema.sql',
+      'source db/seeds.sql',
+      function(err, results, fields) {
+      }
+)
+  db.query(
+    'SELECT * FROM employee',
+    function(err, results, fields) {
+        console.log(results);
+    }
+)
 
-generalPrompts();
+
+
+// function generalPrompts() {
+//     inquirer
+//     .prompt([
+//         {
+//             type: 'list',
+//             name: 'home',
+//             message: 'Employee Tracker Home Screen',
+//             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update Employee Role']
+//         }
+//     ])
+//     .then((data) => {
+
+//     })
+// }
+
+// generalPrompts();
